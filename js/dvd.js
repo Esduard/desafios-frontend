b5946efda4cd99b277b07f260bd70eb120aa5f85
Esduard <42816427+Esduard@users.noreply.github.com>
2020-03-11 19:00:15 -0300

    Logo inicia em local aleatório
    Agora o dvd começa em coordenadas aleatórias de valor entre 0px e o maximo possível daqule eixo, seja x ou y.

--- js/dvd.js
+++ js/dvd.js
@@ -1,13 +1,3 @@
-let dvd = document.getElementById('dvd_logo'); // referencia ao SVG todo
-let background = document.getElementById('container'); // referencia ao fundo
-
-let x = 0; //posicao inical da logo no eixo x
-let y = 0;  //posicao inicial da logo no eixo y
-
-let xspeed = 5; //velocidade horizontal
-let yspeed = 5; //velocidade vertical
-
-
 const colors = "cores.json"
 
 //funcao request color
@@ -18,18 +8,41 @@ function requestColor(){
 
 }
 
+let dvd = document.getElementById('dvd_logo'); // referencia ao SVG todo
+let background = document.getElementById('container'); // referencia ao fundo
+
+let background_width = background.clientWidth; //largura do fundo
+let background_height = background.clientHeight; //altura do fundo
+
 //captura das medidas da logo
 const width = dvd.clientWidth;
 const height = dvd.clientHeight;
 
 
+//inicio aleatorio das coordenadas x e y:
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min; //funcao para obter valor aleatório plausível a posicao inicial da logo
+  }
+
+
+
+let x = getRndInteger(0,background_width - width); //posicao inical da logo no eixo x
+let y = getRndInteger(0,background_height - height);  //posicao inicial da logo no eixo y
+
+//associando a nova posicao ao objeto real em html
+dvd.style.left = x + 'px';
+dvd.style.top = y + 'px';
+
+let xspeed = 3; //velocidade horizontal inicial
+let yspeed = 3; //velocidade vertical inicial
 
 
 function move(){
 
     //captura das medidas do fundo. captura deve acontecer em tempo real para ajustar
-    let background_width = background.clientWidth;
-    let background_height = background.clientHeight;
+    background_width = background.clientWidth;
+    background_height = background.clientHeight;
 
     x += xspeed;
     y += yspeed;
